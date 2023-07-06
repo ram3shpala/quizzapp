e8f66da33ab2efa88126ad695975778a2b1fc650
ram3shpala <ram3shpala@gmail.com>
2023-07-06 16:59:18 +0000

    fix issue

--- src/main/java/com/quizapp/quiz/resources/QuizResource.java
+++ src/main/java/com/quizapp/quiz/resources/QuizResource.java
@@ -3,17 +3,15 @@ package com.quizapp.quiz.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.quizapp.quiz.entity.Quiz;
 import com.quizapp.quiz.service.QuizService;
 
-@RestController
-@RequestMapping("/quiz/")
+@Controller
 public class QuizResource {
 
     private QuizService quizService;
@@ -35,7 +33,6 @@ public class QuizResource {
     @GetMapping("/index")
     public String getHome()
     {
-        System.out.println("Hello Index");
         return "index";
     }
 }
