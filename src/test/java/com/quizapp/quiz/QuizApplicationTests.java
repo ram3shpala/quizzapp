707285dd49b9af0ae67e3c31fdb75d3da7df6a55
ram3shpala <ram3shpala@gmail.com>
2023-07-07 14:00:19 +0000

    update test

--- src/test/java/com/quizapp/quiz/QuizApplicationTests.java
+++ src/test/java/com/quizapp/quiz/QuizApplicationTests.java
@@ -3,7 +3,7 @@ package com.quizapp.quiz;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest	
 class QuizApplicationTests {
 
 	@Test
@@ -11,3 +11,4 @@ class QuizApplicationTests {
 	}
 
 }
+
---
+++ src/test/java/com/quizapp/quiz/QuizResourceTest.java
@@ -0,0 +1,17 @@
+package com.quizapp.quiz;
+@org.springframework.boot.test.context.SpringBootTest
+public class QuizResourceTest {
+    @org.junit.jupiter.api.Test
+    public void contextLoads() {
+    }    
+    
+    @org.junit.jupiter.api.Test
+    public void testGetQuizzes() {
+        assert(true);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testGetQuiz() {
+        assert(true);
+    }
+}
